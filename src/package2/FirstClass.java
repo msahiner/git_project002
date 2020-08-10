5d7ce8972ffaa9555259f50c4d10833ef0cb74ea
Mehmet Sahiner <msahiner@gmail.com>
2020-08-09 21:46:58 -0400

    The fourth commit form the local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
 		System.out.println("This is the second commit from local master");
 		System.out.println("This is the third commit form local master");
 		System.out.println("This is the first commit form local repo");
+		System.out.println("This is the fourth commit form local master");
 		
 
 	}
