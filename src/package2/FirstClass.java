3ef3a38e1ecd128fd358a43d161cf33e7bc1da46
Mehmet Sahiner <msahiner@gmail.com>
2020-08-09 22:30:06 -0400

    The last commit form local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -6,10 +6,10 @@ public class FirstClass {
 		
 		System.out.println("This is the first changes from local master");
 		System.out.println("This is the second commit from local master");
-		System.out.println("This is the third commit form local master");
+		System.out.println("This is the third commit from local master");
 
-		System.out.println("This is the first commit form local repo");
-		System.out.println("This is the fourth commit form local master");
+		System.out.println("This is the first commit from local repo");
+		System.out.println("This is the fourth commit from local master");
 
 		System.out.println("This is the second from remote master");
 
