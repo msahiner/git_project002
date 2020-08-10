f58e962bb622a77a57babc65cc55be27e6625d0f
Mehmet Sahiner <msahiner@gmail.com>
2020-08-09 23:58:13 -0400

    The third commit from local sahiner

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -16,6 +16,7 @@ public class FirstClass {
 		System.out.println("First commit from sahiner branch");
 		
 		System.out.println("The second commit from local sahiner");
+		System.out.println("This is the third commit from local sahiner");
 		
 	}
 
